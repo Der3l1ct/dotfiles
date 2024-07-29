d2df48dd712bcb863c76cb28c0f250f67b90dc5d
CodesAndTokes <127527969+Der3l1ct@users.noreply.github.com>
2024-07-28 22:48:05 -0600

    Update browser.sh
    Changed Firefox install to Mercury

--- .install/browser.sh
+++ .install/browser.sh
@@ -3,30 +3,34 @@
 # ------------------------------------------------------
 if [ -d ~/dotfiles ] ;then
     current_browser=$(cat ~/dotfiles/.settings/browser.sh)
-    if [ ! "$current_browser" == "firefox" ] ;then
+    if [ ! "$current_browser" == "mercury-browser" ] ;then
         echo -e "${GREEN}"
         figlet "Browser"
         echo -e "${NONE}"
         echo ":: The current browser is $current_browser"
-        if gum confirm "Do your want to install Firefox instead?" ;then
-            echo ":: Installing Firefox..."
-            _installPackagesPacman "firefox"
-            echo ":: Setting Firefox as Default browser"
-            echo "firefox" > ~/dotfiles/.settings/browser.sh
-            echo "firefox https://chat.openai.com" > ~/dotfiles/.settings/ai.sh
-            xdg-settings set default-web-browser firefox.desktop
+        if gum confirm "Do your want to install Mercury instead?" ;then
+            echo ":: Installing Mercury..."
+            _installPackagesYay() {
+            yay -S mercury-browser-bin
+            }
+            echo ":: Setting Mercury as Default browser"
+            echo "mercury-browser" > ~/dotfiles/.settings/browser.sh
+            echo "mercury-browser https://chat.openai.com" > ~/dotfiles/.settings/ai.sh
+            xdg-settings set default-web-browser mercury-browser
             echo
             gum spin --spinner dot --title "Recommended to change the browser icon to Firefox in ~/dotfiles/.settings/waybar-quicklinks.json" -- sleep 5
         elif [ $? -eq 130 ]; then
             echo ":: Installation canceled."
             exit 130
         else
-            echo ":: Installation of Firefox skipped"
+            echo ":: Installation of Mercury skipped"
         fi
     fi
 else
-    echo ":: Firefox will be installed as default browser."
-    _installPackagesPacman "firefox"
-    echo ":: Setting Firefox as Default browser"
-    xdg-settings set default-web-browser firefox.desktop
+    echo ":: Mercury will be installed as default browser."
+    _installPackagesYay() {
+    yay -S mercury-browser-bin
+    }
+    echo ":: Setting Mercury as Default browser"
+    xdg-settings set default-web-browser mercury-browser
 fi
